b5fb770cedb0bceef36a8a5fc5c2b89ac4b2d63f
Dmitriy Smishnyi <dmitriysmishnyi@Mac-Pro-Nakitel.local>
2018-07-05 18:11:36 +0300

    lesson #8. Enums

---
+++ src/lesson8/Color.kt
@@ -0,0 +1,11 @@
+package lesson8
+
+enum class Color(var colorHex: String, var opacity: Double? = 1.0) {
+
+    WHITE ("#FFFFFF"),
+    BLACK ("#000000"),
+    RED ("#22rr3333", 0.5),
+    GREEN ("#qweqwee");
+
+
+}
---
+++ src/lesson8/Main.kt
@@ -0,0 +1,9 @@
+package lesson8
+
+fun main(args: Array<String>) {
+
+    println(Color.BLACK)
+    println(Color.BLACK.colorHex)
+    println(Color.BLACK.opacity)
+
+}
